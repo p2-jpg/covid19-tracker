116a9e8f134d46edfb50b8dea5254d85e39bc95f
Patrick Tu <tu.patrick.n@gmail.com>
2020-08-06 13:22:58 -0400

    Updates

---
+++ precache-manifest.fc3d04c537b666b3f170361c6c377c79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1e3396e6c7d3b276baaf8c7cd7eaaf",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "3e5a9de731eb9a45b6d5",
+    "url": "/covid19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "26abc9a5f367e623eb05",
+    "url": "/covid19-tracker/static/css/main.2309596c.chunk.css"
+  },
+  {
+    "revision": "3e5a9de731eb9a45b6d5",
+    "url": "/covid19-tracker/static/js/2.b343015d.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19-tracker/static/js/2.b343015d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26abc9a5f367e623eb05",
+    "url": "/covid19-tracker/static/js/main.6f677a93.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
